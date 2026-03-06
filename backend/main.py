86b9bc469fd1657f3375b0e3ac7c775bd5503985
Luke <LukeSupan@outlook.com>
2026-03-06 16:36:50 -0500

    middleware and fastAPI readded

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,20 @@
-from games.hero_shooter import run as run_hero_shooter # overwatch, marvel rivals, anything like that
+import sys
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware # cors is needed for frontend
+
+app = FastAPI() # create FastAPI app
+
+# add middleware to allow frontend
+# this will be limited later
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+# import game runners
+from games.hero_shooter import run as run_hero_shooter
 from games.lanes import run as run_lanes
 from games.generic import run as run_generic
 from games.generic_versus import run as run_generic_versus
@@ -7,20 +23,9 @@ from games.lanes_detailed import run as run_lanes_detailed
 from games.hero_shooter_versus import run as run_hero_shooter_versus
 from games.one_vs_one import run as run_one_vs_one
 
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-
-# currently implemented tags
-# they all expect different results
+# from games.deadlock import run as run_deadlock # could include other games with 3 lanes TODO
+# from games.generic import run as run_generic # this is all generic games with no roles. less info but, it does the job
 GAME_RUNNERS = {
     "hero_shooter": run_hero_shooter,
     "hero_shooter_versus": run_hero_shooter_versus,
@@ -36,14 +41,9 @@ GAME_RUNNERS = {
     "one_vs_one": run_one_vs_one,
 }
 
+
 @app.post("/stats")
 def get_stats(payload: dict):
-    lines = payload["lines"]  # get raw lines from frontend
-    game_name = lines[0].lower().strip()
-    games = lines[1:]
 
-    runner = GAME_RUNNERS.get(game_name)
-    if not runner:
-        raise HTTPException(status_code=400, detail=f"{game_name} is not valid")
-    
-    return runner(games)
+
+    return {}
